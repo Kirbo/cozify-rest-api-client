98bac95c2f4653964e67af1951fba640fbede76a
Kimmo Saari <kirbownz@me.com>
2018-08-29 01:55:47 +0300

    Progress

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,14 @@
 /**
  * Generic functions are defined here.
  */
+import * as os from 'os';
+import * as path from 'path';
 
 import { IAxiosError } from './types';
+
+const GET_BASE_DIR = (): string => (
+  path.resolve(`${os.homedir()}/.config/cozify-rest-api-client`)
+);
 
 const CATCH_ERROR = (error: IAxiosError) => {
   throw new Error(error);
@@ -10,4 +16,5 @@ const CATCH_ERROR = (error: IAxiosError) => {
 
 export {
   CATCH_ERROR,
+  GET_BASE_DIR,
 };
